6e118a97c102ebff12fa32b0735b0ad4120e215b
MartinoLeone-Reg <90612894+MartinoLeone-Reg@users.noreply.github.com>
2021-10-04 09:40:43 +0200

    Refactoring

--- express/app.js
+++ express/app.js
@@ -1,15 +1,10 @@
 var mysql = require('mysql');
 
 module.exports={
-    saldiMultiRemove:function(result,quantity,date){
+    queryProduct:function(result,quantity,date){
         let res=[[]];
-        //product
-        //cost
-        //shippig time
-        //suppiler
-        //console.log(result);
+        
         date = Date.parse(date);
-        //console.log("entro");
         let b=0;
         let c=0;
         result[result.length]="";
@@ -17,41 +12,24 @@ module.exports={
         for(let i=0;i<result.length;i++){
             var startPromotion = Date.parse(result[i]['startPromotion']);
             var endPromotion = Date.parse(result[i]['endPromotion']);
-            let cost=result[i]['price']*quantity;
-            //console.log("no",result[i]['productName'])
-            //console.log(quantity,result[i]['minPiece'])
-            //console.log(result[i]['price']*quantity,"",result[i]['orderValue'])
-            //console.log(startPromotion,"",date)
-            //console.log(endPromotion,">=",date)
-              
+            let cost=result[i]['price']*quantity;  
             b=i;
             let check=true;
             let promotion=0;
-            //console.log(quantity>=result[b]['minPiece'] )
-            //console.log(result[i]['price']*quantity>=result[b]['orderValue'])
-            //console.log(startPromotion<=date &&  endPromotion>=date)
 
             if(quantity>=result[b]['minPiece'] && result[i]['price']*quantity>=result[b]['orderValue'] && startPromotion<=date &&  endPromotion>=date){
-                //console.log("i",i);
-                //console.log(result[b]['productName'],"==",result[i]['productName'],"&&",result[b]['name'],"==",result[i]['name'],"&&")
-                    if(i<result.length-1 && result[b]['productName']==result[i]['productName'] && result[b]['name']==result[i]['name']){
+                
+                if(i<result.length-1 && result[b]['productName']==result[i]['productName'] && result[b]['name']==result[i]['name']){
 
                         while(i<result.length-1 && result[b]['productName']==result[i]['productName'] && result[b]['name']==result[i]['name'] && check==true){
 
                             if(result[i]['function']==0){
-                                //console.log(result[i]['name']," prova ",cost,"-",(cost/100),"*",(result[i]['discount']));
-                                //console.log(result[i]['function'])4
-                                //cost=cost-(cost/100)*(result[i]['discount']);
                                 cost=discount(cost,result[i]['discount'],quantity,result[i]['function'])
                                 promotion=promotion+result[i]['discount'];
                             }else{
-                                //console.log(cost,">",result[i]['price']*quantity-((result[i]['price']*quantity)/100)*result[i]['discount'])
                                 if(cost>result[i]['price']*quantity-((result[i]['price']*quantity)/100)*result[i]['discount']){
-                                        //console.log('entro:',result[i]['price'])
-                                        //console.log(result[i]['price']*quantity,"-",(result[i]['price']/100),"*",result[i]['discount'])
-                                        //cost=result[i]['price']*quantity-((result[i]['price']*quantity)/100)*result[i]['discount'];
-                                        cost=discount(result[i]['price'],result[i]['discount'],quantity,result[i]['function'])
-                                        //console.log(cost);
+                                        
+                                    cost=discount(result[i]['price'],result[i]['discount'],quantity,result[i]['function'])
                                         promotion=result[i]['discount'];
 
                                     }
@@ -63,7 +41,6 @@ module.exports={
                         }   
                      i--;
                     }else{
-                        //console.log("nop");
                         cost=cost-(cost/100)*(result[i]['discount']);
                         promotion=promotion+result[i]['discount'];
 
@@ -76,12 +53,13 @@ module.exports={
             c++
             }   
         }
-        //console.log(res);
         res.sort(sortByPrice)
         return res;
     },
     discount
 }
+
+
 function sortByPrice(a,b){
     if (a[2] === b[2]) {
         return 0;
@@ -91,6 +69,7 @@ function sortByPrice(a,b){
     }
 
 }
+
 
 function discount(cost,discount,quantity,func) {
     if(func==0){
--- express/index.js
+++ express/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const express = require('Express');
 const app=express();
 const port =3000;
-const saldiMultiRemove = require('./app').saldiMultiRemove;
+const saldiMultiRemove = require('./app').queryProduct;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,21 +13,12 @@ app.get('/',(req,res)=>{
     console.log("running");
 });
 
-app.get("/test1", function (req, res) {
-    let arr=[1,2,3]
-    res.send(arr)
-});
-app.get("/test2/:id", function(req,res){
-    console.log(req.params.id)
-})
+
 app.get("/query/:search/:date/:quantity",function(req,res){
     let product=req.params.search;
     let date=req.params.date;
     let quantity= req.params.quantity;
     console.log(product)
-        //let quantity=12;
-        //let date='2021-09-24';
-        //let query="SELECT * FROM supplier,owned,stock,has,promotion WHERE stock.productName LIKE '%"+product+"%' AND owned.idSt=stock.idSt AND supplier.idS=owned.idS AND owned.quantity>"+quantity+" AND owned.idO=has.idO AND has.idPromotion=promotion.idPromotion AND promotion.startPromotion<='"+date+"' AND promotion.endPromotion>='"+date+"' ORDER BY owned.shippingTime";
         let query="SELECT * FROM supplier,owned,stock,has,promotion WHERE stock.productName LIKE '%"+product+"%' AND owned.idSt=stock.idSt AND supplier.idS=owned.idS AND owned.quantity>="+quantity+" AND owned.idO=has.idO AND has.idPromotion=promotion.idPromotion ORDER BY owned.shippingTime";
 
         var mysql = require('mysql');
@@ -43,7 +34,6 @@ app.get("/query/:search/:date/:quantity",function(req,res){
         con.query(query, function (err, result, fields) {
             if (err) throw err;
                 var resu=saldiMultiRemove(result,quantity,date);
-                //res.send(resu[0][2]);
                 res.send(resu);
 
                 con.end();
